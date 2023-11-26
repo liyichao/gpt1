29bfa7837bfef22ef5369488c54f3baaa9016627
lyc <liyichao.good@gmail.com>
2023-11-26 16:30:14 +0000

    add checkpoint for tokens.

--- main.py
+++ main.py
@@ -9,10 +9,14 @@ import torch.optim as optim
 from torch.utils.data import TensorDataset, random_split
 from datasets import load_dataset, Dataset
 from torch.utils.data.sampler import Sampler
+import logging
+import os
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s %(levelname)s %(message)s')
 
 device = 'cuda'
-batch_size = 64
-
+batch_size = 24
+checkpoint_dir = './checkpoint'
 
 
 def train(model: nn.Module, train_generator, dev_generator, tokenizer):
@@ -20,6 +24,7 @@ def train(model: nn.Module, train_generator, dev_generator, tokenizer):
     criterion = torch.nn.NLLLoss()
     optimizer = optim.Adam(model.parameters())
 
+    print_batch = 100
     for epoch in range(1):
         running_loss = 0.0
         model.train()
@@ -33,8 +38,8 @@ def train(model: nn.Module, train_generator, dev_generator, tokenizer):
             loss.backward()
             optimizer.step()
             running_loss += loss.item()
-            if i % 10 == 9:
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 10:.3f}')
+            if i % print_batch == (print_batch - 1):
+                logging.info(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 10:.3f}')
                 running_loss = 0.0
         #print("epoch %d, test accurary: %f" % (epoch, evaluate(model, dev_generator)))
 
@@ -64,8 +69,13 @@ def batchify(generator, batch_size):
             yield out
             i = 0
         
-def preprocess(dataset, tokenizer):
-    tokens = dataset.map(lambda x: {"text": tokenizer.encode(x['text']).ids})
+def preprocess(dataset, tokenizer, token_saved_file):
+    if not os.path.exists(token_saved_file):
+        tokens = dataset.map(lambda x: {'text': tokenizer.encode(x['text']).ids}, num_proc=16)
+        logging.debug('dataset `%s`, encoded `%s`', dataset[0]['text'], tokens[0]['text'])
+        tokens.save_to_disk(token_saved_file)
+    else:
+        tokens = Dataset.load_from_disk(token_saved_file)
     def batch_iterator():
         target_tokens = 512
         next_sample = []
@@ -77,20 +87,30 @@ def preprocess(dataset, tokenizer):
                 next_sample = token['text'][to_concat:]
     return batchify(batch_iterator, batch_size)
 
-def main():
-    tokenizer = Tokenizer.from_file('./byte-level-bpe.tokenizer.json')
-    dataset = load_dataset("bookcorpus", split="train")
-    dataset = dataset.select(range(0,1000000))
-    #dataset.to_json('./bookcorpus.json')
-    #dataset = Dataset.from_json('./bookcorpus.json')
-    train_size = math.floor(0.7 * len(dataset)) 
-    train_set = dataset.select(range(0, train_size))
-    dev_set = dataset.select(range(train_size, len(dataset) - train_size))
-    train_generator = preprocess(train_set, tokenizer)
-    dev_generator = preprocess(dev_set, tokenizer)
-    model = GPT1(d_model=2, nhead=2, num_layers=2, device=device, vocab_size=tokenizer.get_vocab_size())
-    train(model, train_generator, dev_generator, tokenizer)
 
+def load_dataset():
+    tokenizer = Tokenizer.from_file('./byte-level-bpe.tokenizer.json')
+    dev_tokens = checkpoint_dir + '/dev_tokens' 
+    train_tokens = checkpoint_dir + '/train_tokens' 
+    if not os.path.exists(dev_tokens):
+        dataset = load_dataset("bookcorpus", split="train")
+        #dataset = dataset.select(range(0,1000000))
+        train_size = math.floor(0.7 * len(dataset)) 
+        train_set = dataset.select(range(0, train_size))
+        dev_set = dataset.select(range(train_size, len(dataset) - train_size))
+    else:
+        train_set = None
+        dev_set = None
+    train_generator = preprocess(train_set, tokenizer, train_tokens)
+    dev_generator = preprocess(dev_set, tokenizer, dev_tokens)
+    return train_generator, dev_generator, tokenizer
+        
+
+def main():
+    train_generator, dev_generator, tokenizer = load_dataset()
+    model = GPT1(device=device, vocab_size=tokenizer.get_vocab_size())
+    logging.info('start training')
+    train(model, train_generator, dev_generator, tokenizer)
 
 
 if __name__ == '__main__':
